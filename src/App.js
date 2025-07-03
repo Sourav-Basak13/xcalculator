afdded757eccfc7a926bb582e11a6572a961db52
Sourav Basak <sourav.new2016@gmail.com>
2025-07-04 00:36:12 +0530

    completed xcalculator

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
     >
       <h1>React Calculator</h1>
       <input type="text" name="calculator_input" value={inputStr} readOnly />
-      <div className="cal_result">{result}</div>
+      <p className="cal_result">{result}</p>
       <span className="btn_wrapper">
         {[7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "*", "C", 0, "=", "/"].map(
           (_ele) => (
